267163076710ea4ce66bc4c2fa08cd66185e32da
s5664111 <117274748+s5664111@users.noreply.github.com>
2022-12-12 19:36:06 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+#include "lib.h"
+float massimo(float numeri[], int dimensione){
+  float massimo=0;
+  for(int i=0;i<dimensione;i++){
+    if(massimo<numeri[i]){
+      massimo=numeri[i];
+    }
+  }
+  return massimo;
+}

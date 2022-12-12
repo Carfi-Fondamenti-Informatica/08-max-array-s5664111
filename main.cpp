f8db0b319354bf2914ab55b5e7dd8f89b2daaf9c
s5664111 <117274748+s5664111@users.noreply.github.com>
2022-12-12 19:38:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+#include"lib.h"
+using namespace std;
 int main(){
+  int dimensione=0;
+  cin>>dime nsione;
+  float numeri[dimensione];
+  for(int i=0; i<dimensione;i++){
+    cin>>numeri[i];
+  }
+  cout<<massimo(numeri,dimensione)<<endl;
   return 0;
 }
